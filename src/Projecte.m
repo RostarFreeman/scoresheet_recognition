367f39f11d1eca81cb90b57b628e266dfc82ebe0
Pau Torras <57144976+RostarFreeman@users.noreply.github.com>
2020-05-05 20:01:42 +0200

    Current state
    Getting a score sheet picture and erasing the background staff.

---
+++ src/Projecte.m
@@ -0,0 +1,8 @@
+% % % % % % % % % % % % % % % %
+% Projecte                    %
+% Pau Torras Coloma - 1495172 %
+% Marc Espinosa Gil - 1495036 %
+% % % % % % % % % % % % % % % %
+
+close all; clearvars; clc;
+
